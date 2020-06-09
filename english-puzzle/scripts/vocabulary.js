e0d783b2bb77ee00087d9a0b6aa72b5a0aea967d
Pavel Khatyliou <hatylevpavel@gmail.com>
2020-06-09 22:40:17 +0300

    feat: add more details in layout

---
+++ english-puzzle/scripts/vocabulary.js
@@ -0,0 +1,12 @@
+const vocabulary = [
+    {
+        sentence: 'Fuck you asshole',
+        translation: 'Пошёл на хуй ублюдок'
+    },
+    {
+        sentence: 'Hello my little extremism lovers',
+        translation: 'Здравствуйте, мои маленькие любители экстремизма'
+    }
+];
+
+export default vocabulary;
