df6d9ef6486e60541e3dfce34655c678ea7a47a7
Pavel Khatyliou <hatylevpavel@gmail.com>
2020-06-10 20:03:39 +0300

    feat: add vocabulary

--- english-puzzle/scripts/main.js
+++ english-puzzle/scripts/main.js
@@ -6,12 +6,28 @@ import Unsplash from './UnsplashAPI.js';
 import YandexAPI from './YandexAPI.js';
 import selectors from './Selectors.js';
 
-class Controller {
-  constructor() {
+// class Controller {
+//   constructor() {
 
+//   }
+
+
+// }
+
+const btnStart = document.querySelector('.btn__start');
+
+class MenuNavigation {
+  constructor() {
+    this.startScreen = document.querySelector('.start-screen');
+    this.gameScreen = document.querySelector('.game-screen');
   }
 
-
+  gameStart() {
+    this.startScreen.classList.remove('start-screen_active');
+    this.gameScreen.classList.add('game-screen_active');
+  }
 }
 
-const controller = new Controller();
+const menuNavigation = new MenuNavigation();
+
+btnStart.onclick = menuNavigation.gameStart.bind(menuNavigation);
