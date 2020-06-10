7e7b98a82291ac1e82de3a3f22934f1af0494e78
Pavel Khatyliou <hatylevpavel@gmail.com>
2020-06-10 20:44:19 +0300

    feat: add sentence generation

--- english-puzzle/scripts/main.js
+++ english-puzzle/scripts/main.js
@@ -4,15 +4,45 @@ import Clock from './Clock.js';
 import Weather from './WeatherAPI.js';
 import Unsplash from './UnsplashAPI.js';
 import YandexAPI from './YandexAPI.js';
-import selectors from './Selectors.js';
+import book1 from '../assets/vocabulary/data/book1.js';
 
-// class Controller {
-//   constructor() {
+class Controller {
+  constructor() {
+    this.book1 = book1;
+    this.sentence = document.querySelector('.game-screen__sentence');
+    this.pieces = document.querySelector('.game-screen__puzzle-pieces');
 
-//   }
+    this.currentSentence = 0;
+  }
 
+  shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
 
-// }
+  createSentence() {
+    this.sentence.innerHTML = this.book1[this.currentSentence].textExampleTranslate;
+  }
+
+  createTranslation() {
+    let engSentence = this.book1[this.currentSentence].textExample.split(' ');
+    this.shuffle(engSentence)
+
+    for (let word of engSentence) {
+      let newElem = document.createElement('div');
+      newElem.classList.add('puzzle-piece');
+      newElem.innerHTML = word;
+      this.pieces.append(newElem);
+    }
+  }
+
+  start() {
+    this.createSentence();
+    this.createTranslation();
+  }
+}
 
 const btnStart = document.querySelector('.btn__start');
 
@@ -29,5 +59,8 @@ class MenuNavigation {
 }
 
 const menuNavigation = new MenuNavigation();
+const controller = new Controller();
+
+controller.start();
 
 btnStart.onclick = menuNavigation.gameStart.bind(menuNavigation);
